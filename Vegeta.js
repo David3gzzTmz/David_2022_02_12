37958c70fc646021d3df321080229baf344f76d2
David3gzzTmz <100738543+David3gzzTmz@users.noreply.github.com>
2022-03-15 20:25:43 -0600

    Create Vegeta.js

---
+++ Vegeta.js
@@ -0,0 +1,32 @@
+var app = new Vue({
+	el: '#app',
+		data: {
+			message : 'Hello Vue!'
+		}
+});
+
+var app2 = new Vue({
+	el: '#app2',
+		data: {
+			bucl:[
+				{message: "Hola"},
+				{message: "¿No tienes entradas en Mercados Libros Tec? "},
+				{message: "Yo si tengo"},
+				
+			]
+		}
+});
+
+var app3 = new Vue({
+	el: '#app3',
+		data: {
+				nombre:'David',
+				fecha:'15 de Marzo',
+				clase:'Desarrollo de apps web',
+		}
+});
+
+var Vegeta = new Vue({
+data:	{
+vegeta: 'https://static.wikia.nocookie.net/dragonball/images/c/cc/Vegeta7497237498723jfsdkfhjksdsfsd.jpg/revision/latest?cb=20131028231038&path-prefix=es'
+}});

3f37f5262ea9aa8f552cda8c23463f79630fc555
David3gzzTmz <100738543+David3gzzTmz@users.noreply.github.com>
2022-03-15 21:41:45 -0600

    Update Vegeta.js

--- Vegeta.js
+++ Vegeta.js
@@ -1,7 +1,7 @@
 var app = new Vue({
 	el: '#app',
 		data: {
-			message : '¡Bienvenidos a Vegeta Oficial!'
+			message : '¡Bienvenidos a Vegeta oficial la página oficial de Vegeta!'
 		}
 });
 
@@ -11,7 +11,7 @@ var app2 = new Vue({
 			bucl:[
 				{message: "Hola"},
 				{message: "¿No tienes entradas en Mercados Libros Tec? "},
-				{message: "Yo si tengo"},
+				{message: "Yo si tengo"}
 				
 			]
 		}
@@ -22,11 +22,12 @@ var app3 = new Vue({
 		data: {
 				nombre:'David',
 				fecha:'15 de Marzo',
-				clase:'Desarrollo de apps web',
+				clase:'Desarrollo de apps web'
 		}
 });
 
-var Vegeta = new Vue({
+var iVegeta = new Vue({
+el: '#iVegeta',
 data:	{
 vegeta: 'https://i.quotev.com/img/q/u/15/6/11/cad6aeaf90-imag.jpg'
 }});

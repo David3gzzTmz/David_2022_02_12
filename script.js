6e553c6ea0ed0736423ec5699441303330b624fc
David3gzzTmz <100738543+David3gzzTmz@users.noreply.github.com>
2022-04-09 23:00:12 -0500

    Update script.js

--- script.js
+++ script.js
@@ -11,6 +11,7 @@ var app2 = new Vue({
     message: 'Usted cargó esta página el ' + new Date().toLocaleString()
   }
 })
+
 // Loop (v-if)
 var api = new Vue({
       el: '#apex',
@@ -66,4 +67,92 @@ var Youtube=new Vue({
 			{ text: 'Cocina', value: 'Adam Raguesa' }
     ]
   }
+})
+
+//User Input (v-model)
+var Instrumentos= new Vue({
+  el: '#Instrum',
+  methods: {
+    di: function (mensaje) {
+      alert(mensaje)
+    }
+  }
+})
+
+
+
+
+var Perdi = new Vue({
+  el: '#per',
+  data: {
+    Perdi_el_tiempo: 'si'
+  },
+  computed: {
+    Triste: function () {
+      return this.Perdi_el_tiempo + ' ' +'Perdi el tiempo'+ ' Que triste';
+    }
+  }
+})
+
+var aumento = new Vue({
+  el: '#Mond',
+  data: {
+    counter: 2,
+		counter2: 1
+  }
+})
+/*
+Falle = new Vue({
+  el: '#F',
+  data: {
+    posts: [
+      { a: 1, title: 'Falle' },
+      { a: 2, title: 'en todas' },
+      { a: 3, title: 'mis actividades' }
+    ]
+  }
+}) */
+
+var dificultad = new Vue({
+  el: '#dif',
+   data: {
+    message: 'Seleccióna dificultad',
+		difi:'Normal'
+  },
+  computed: {
+     Dificult: function () {
+      // `this` apunta a la instancia vm
+    if (this.difi =='Normal') {
+   return 'Escudos al 80%; Norwood 2 al 3, no se requiere tanto medicamento quizas minoxidil';
+} else {
+   if (this.difi =='Heroica') {
+ return 'Escudos al 50%; Norwood 3 vertrex, 4 o 5 leve, requieres minoxidil y quizas finasterida, considera implantes de cabellos';
+} else {
+   if (this.difi =='Legendaria') {
+   return 'Escudos; al 15% Norwood 5 al 7, sera necesario cirujia además del los medicamentos, quizas debas resignarte y raparte la cabellera';
+} else {
+   if (this.difi =='Facil') {
+   return 'Escudos al 100%; Norwood 1 al 2, no se requiere nada, eres un jovenazo';
+					}
+				}
+			}
+		}
+			 return message;
+  }
+}})
+
+
+
+lDOM = new Vue({
+	el: '#ldom',
+	data(){
+		return{
+		message:'no se que es DOM'}
+	},
+  methods:{
+		pressed(){
+			let text = this.$refs.app.message;
+			alert(text + 'from $refs');
+		}
+	}
 })
